0a15022efa273586e3727f9a493c516136bcfa19
shengui919 <shengui919@gmail.com>
2022-03-21 08:48:23 +0800

    first version

--- src/public/app.js
+++ src/public/app.js
@@ -1,1 +1,28 @@
 // Put all your frontend code here.
+const messageList = document.querySelector('ul');
+const nickForm = document.querySelector('#nick');
+const messageForm = document.querySelector('#message');
+const socket = new WebSocket(`ws://${window.location.host}`);
+socket.addEventListener('open', () => {
+  console.log('Connected to Server ✅');
+});
+socket.addEventListener('message', (message) => {
+  const li = document.createElement('li');
+  li.innerText = message.data;
+  messageList.append(li);
+});
+socket.addEventListener('close', () => {
+  console.log('Disconnected from Server ❌');
+});
+const makeMessage = (type, payload) => JSON.stringify({ type, payload });
+messageForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const input = messageForm.querySelector('input');
+  socket.send(makeMessage('message', input.value));
+  input.value = '';
+});
+nickForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const input = nickForm.querySelector('input');
+  socket.send(makeMessage('nick', input.value));
+});
--- src/server.js
+++ src/server.js
@@ -1,20 +1,40 @@
-import http from "http";
-import WebSocket from "ws";
-import express from "express";
+import http from 'http';
+import WebSocket, { WebSocketServer } from 'ws';
+import express from 'express';
 
 const app = express();
 
-app.set("view engine", "pug");
-app.set("views", __dirname + "/views");
-app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (_, res) => res.render("home"));
-app.get("/*", (_, res) => res.redirect("/"));
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use('/public', express.static(__dirname + '/public'));
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.redirect('/'));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-
+const handleListen = () => console.log(`Listen on http://localhost:3000`);
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const wss = new WebSocketServer({ server });
 
-// Put all your backend code here.
+const sockets = [];
+wss.on('connection', (socket) => {
+  sockets.push(socket);
+  socket['nickname'] = 'Guest';
+  console.log('Connected to Browser ✅');
+  socket.on('close', () => console.log('Disconnected from Browser ❌'));
+  socket.on('message', (message) => {
+    const msg = JSON.parse(message);
+    switch (msg.type) {
+      case 'nick':
+        socket['nickname'] = msg.payload;
+        break;
+      case 'message':
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket['nickname']}: ${msg.payload}`)
+        );
+        break;
+      default:
+        break;
+    }
+  });
+});
 
-server.listen(process.env.PORT, handleListen);
+server.listen(process.env.PORT || 3000, handleListen);
